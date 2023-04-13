fa5b0e30a23105209b56bf53403efcbff7d1f45f
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-04-13 16:54:10 +0700

    Update mapping lsp

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,24 +5,40 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>s"] = { ":w<cr>", "save" },
+    ["<leader>q"] = { ":q<cr>", "quit" },
     ["<leader><leader>"] = { "<cmd> Telescope find_files <CR>", "find files" },
     -- vim tab navigation
     ["tt"] = { ":tabclose<cr>", "tab close", opts = { nowait = true } },
     ["tn"] = { ":tabnew<cr>", "tab new", opts = { nowait = true } },
     ["tk"] = { ":tabnext<cr>", "tab next", opts = { nowait = true } },
     ["tj"] = { ":tabprev<cr>", "tab prev", opts = { nowait = true } },
-    -- hop configuration
+  },
+}
+
+M.hop = {
+  n = {
     ["f"] = {
       ":lua require('hop').hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
       "find current line forward",
     },
     ["F"] = {
       ":lua require('hop').hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
-      "find current line backword",
+      "find current line backward",
     },
-    ["s"] = {
-      ":HopWord<cr>",
-      "find word",
+    ["s"] = { ":HopWord<cr>", "find word" },
+  },
+}
+
+M.lspconfig = {
+  n = {
+    ["<leader>lq"] = { vim.diagnostic.setloclist, "diagnostic setloclist" },
+    ["<leader>la"] = { vim.lsp.buf.code_action, "code action" },
+    ["<leader>lr"] = { vim.lsp.buf.rename, "rename" },
+    ["<leader>lf"] = {
+      function()
+        vim.lsp.buf.format { async = true }
+      end,
+      "format",
     },
   },
 }
