139eb192db4047064cabb930d91fa03dcdbde522
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-04-13 15:56:06 +0700

    Add telescope mapping

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -62,6 +62,8 @@ M.telescope = {
     mappings = {
       i = {
         ["<esc>"] = require("telescope.actions").close,
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_next,
       },
     },
   },
