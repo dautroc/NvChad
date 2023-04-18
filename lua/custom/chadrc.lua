3872628b656ef8f979dd229344880a05f6c1b9a1
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-04-18 07:56:36 +0700

    Update ui

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,11 +5,14 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "doomchad",
-  theme_toggle = { "doomchad", "one_light" },
+  theme = "everforest_light",
+  theme_toggle = { "everforest_light", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  nvdash = { load_on_startup = true },
+  cheatsheet = { theme = "simple" },
 }
 
 M.plugins = "custom.plugins"
