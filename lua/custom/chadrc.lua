415ee11544286a146225b0cfd7d962e1ea6fa6ed
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-04-19 11:50:49 +0700

    Update dash

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "doomchad",
-  theme_toggle = { "doomchad", "one_light" },
+  theme = "everforest_light",
+  theme_toggle = { "everforest_light", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
@@ -14,6 +14,19 @@ M.ui = {
   nvdash = {
     load_on_startup = true,
     buttons = {},
+    header = {
+      [[      ___           ___           ___           ___                       ___     ]],
+      [[     /\  \         /\  \         /\  \         /\__\          ___        /\  \    ]],
+      [[    /::\  \       /::\  \       /::\  \       /:/  /         /\  \      /::\  \   ]],
+      [[   /:/\ \  \     /:/\:\  \     /:/\:\  \     /:/__/          \:\  \    /:/\:\  \  ]],
+      [[  _\:\~\ \  \   /:/  \:\  \   /::\~\:\  \   /::\  \ ___      /::\__\  /::\~\:\  \ ]],
+      [[ /\ \:\ \ \__\ /:/__/ \:\__\ /:/\:\ \:\__\ /:/\:\  /\__\  __/:/\/__/ /:/\:\ \:\__\]],
+      [[ \:\ \:\ \/__/ \:\  \ /:/  / \/__\:\/:/  / \/__\:\/:/  / /\/:/  /    \/__\:\/:/  /]],
+      [[  \:\ \:\__\    \:\  /:/  /       \::/  /       \::/  /  \::/__/          \::/  / ]],
+      [[   \:\/:/  /     \:\/:/  /         \/__/        /:/  /    \:\__\          /:/  /  ]],
+      [[    \::/  /       \::/  /                      /:/  /      \/__/         /:/  /   ]],
+      [[     \/__/         \/__/                       \/__/                     \/__/    ]],
+    }
   },
 
   cheatsheet = { theme = "simple" },
