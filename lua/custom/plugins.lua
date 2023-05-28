8f3463e50787129658b30fedf75396ca9190e827
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-05-28 11:33:52 +0700

    Update oil

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -259,7 +259,23 @@ local plugins = {
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup()
+      require("oil").setup {
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-s>"] = "actions.select_vsplit",
+          ["<C-h>"] = "actions.select_split",
+          ["<C-t>"] = "actions.select_tab",
+          ["<C-p>"] = "actions.preview",
+          ["q"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["g."] = "actions.toggle_hidden",
+        },
+      }
     end,
   },
 
