811292c7d2abff87c2efb97b561d07a2da482d2d
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-07 16:38:53 +0700

    Add fzf native

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -315,6 +315,11 @@ local plugins = {
       end, 0)
     end,
   },
+
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  },
 }
 
 return plugins
